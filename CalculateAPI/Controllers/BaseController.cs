cbf8833005db60ac9d006cade441314704e35942
BATUHAN <BATUHAN@DESKTOP-9MKVHE2>
2022-02-24 09:42:15 +0300

    Yenilik Eklendi

--- CalculateAPI/Controllers/BaseController.cs
+++ CalculateAPI/Controllers/BaseController.cs
@@ -12,35 +12,35 @@ namespace CalculateAPI.Controllers
         Business business = new Business();
 
         [HttpGet, Route("/Gizem/{a}/{b}")]
-        public JsonResult Gizem(double a, double b)
+        public JsonResult Çarpma(double a, double b)
         {
             double c = business.Sum(a, b);
             return Json(c);
         }
 
         [HttpGet, Route("/Batuhan/{a}/{b}")]
-        public JsonResult Batuhan(double a, double b)
+        public JsonResult Toplama(double a, double b)
         {
             double c = business.Multiple(a, b);
             return Json(c);
         }
 
         [HttpGet, Route("/Atakan/{a}/{b}")]
-        public JsonResult Atakan(double a, double b)
+        public JsonResult Çıkarma(double a, double b)
         {
             double c = business.Extraction(a, b);
             return Json(c);
         }
 
         [HttpGet, Route("/Mine/{a}/{b}")]
-        public JsonResult Mine(double a, double b)
+        public JsonResult ÜssüAlma(double a, double b)
         {
             double c = business.BaseOperation(a, b);
             return Json(c);
         }
 
         [HttpGet,Route("/Merve/{a}/{b}")]
-        public JsonResult Merve(double a, double b)
+        public JsonResult Bölme(double a, double b)
         {
             double c = business.Divide(a, b);
             return Json(c);
