f6e1f96e89793ef7f15471ad83a4e0acc85da7fa
BATUHAN <BATUHAN@DESKTOP-9MKVHE2>
2022-02-24 09:36:48 +0300

    Proje dosyası ekle.

---
+++ CalculateAPI/Business.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CalculateAPI
+{
+    public class Business
+    {
+        public double Multiple(double a, double b)
+        {
+            return a * b;
+        }
+        public double Sum(double a, double b)
+        {
+            return a + b;
+        }
+        public double Extraction(double a, double b)
+        {
+            return a - b;
+        }
+
+        public double BaseOperation(double a, double b)
+        {
+            return Math.Pow(a, b);
+        }
+
+        public double Divide(double a, double b)
+        {
+            return a / b;
+        }
+
+    }
+}
---
+++ CalculateAPI/Controllers/BaseController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Threading.Tasks;
+
+namespace CalculateAPI.Controllers
+{
+    public class BaseController : Controller
+    {
+        Business business = new Business();
+
+        [HttpGet, Route("/Gizem/{a}/{b}")]
+        public JsonResult Gizem(double a, double b)
+        {
+            double c = business.Sum(a, b);
+            return Json(c);
+        }
+
+        [HttpGet, Route("/Batuhan/{a}/{b}")]
+        public JsonResult Batuhan(double a, double b)
+        {
+            double c = business.Multiple(a, b);
+            return Json(c);
+        }
+
+        [HttpGet, Route("/Atakan/{a}/{b}")]
+        public JsonResult Atakan(double a, double b)
+        {
+            double c = business.Extraction(a, b);
+            return Json(c);
+        }
+
+        [HttpGet, Route("/Mine/{a}/{b}")]
+        public JsonResult Mine(double a, double b)
+        {
+            double c = business.BaseOperation(a, b);
+            return Json(c);
+        }
+
+        [HttpGet,Route("/Merve/{a}/{b}")]
+        public JsonResult Merve(double a, double b)
+        {
+            double c = business.Divide(a, b);
+            return Json(c);
+        }
+        
+
+
+
+
+
+
+    }
+}
